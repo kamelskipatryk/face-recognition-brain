8d5f42ae4a2331023c9bd882f85cad73bd6b161a
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-07-08 21:27:13 +0200

    adding a functionality to change the sign page to home page

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Signin = () => {
+const Signin = ({onRouteChange}) => {
     return(
         <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
             <main className="pa4 black-80">
@@ -15,14 +15,16 @@ const Signin = () => {
                         <label className="db fw6 lh-copy f6" for="password">Password</label>
                         <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
                     </div>
-                    
                     </fieldset>
                     <div className="">
-                    <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+                    <input
+                    onClick={() => onRouteChange('home')}
+                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                    type="submit"
+                    value="Sign in" />
                     </div>
                     <div className="lh-copy mt3">
                     <a href="#0" className="f6 link dim black db">Register</a>
-                    
                     </div>
                 </form>
             </main>
