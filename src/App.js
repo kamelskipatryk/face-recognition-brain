88fbcbec5f3e274800d8576d77fce6b2df309985
kamelskipatryk <39456685+kamelskipatryk@users.noreply.github.com>
2018-07-08 21:50:31 +0200

    change the conditions statement in render function

--- src/App.js
+++ src/App.js
@@ -77,14 +77,18 @@ class App extends Component {
               params={particlesOptions}
             />
         <Navigation onRouteChange={this.onRouteChange}/>
-        { this.state.route === 'signin'
-          ? <Signin onRouteChange={this.onRouteChange}/>
-          : <div>
+        { this.state.route === 'home'
+          ? <div>
               <Logo />
               <Rank />
               <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
               <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
             </div>
+          : (
+            this.state.route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange}/>
+            : <Register onRouteChange={this.onRouteChange}/>
+          ) 
         }
       </div>
     );
